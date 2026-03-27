420c7a3f6ad9b198c3798ab15697ced36151fcba
BartekK022 <baklu100508@gmail.com>
2026-03-27 13:25:51 +0100

    zadanieNaTrzy

--- app/src/main/java/com/example/projekt2703/MainActivity.java
+++ app/src/main/java/com/example/projekt2703/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.projekt2703;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    ArrayList<String> arrayListowoce;
+    ArrayAdapter<String> arrayAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        listView = findViewById(R.id.listView);
+        arrayListowoce = new ArrayList<>();
+        arrayListowoce.add("banan");
+        arrayListowoce.add("arbuz");
+        arrayListowoce.add("malina");
+        arrayListowoce.add("truskawski");
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,arrayListowoce);
+        listView.setAdapter(arrayAdapter);
+
     }
 }
